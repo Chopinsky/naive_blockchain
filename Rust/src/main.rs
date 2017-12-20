dba976b0a1e02a875e3d1d296b8b1b08361bc232
Jacob Zuo <chopinsky@live.com>
2017-12-19 20:43:25 -0600

    make rust to read public key file

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,20 +1,41 @@
 extern crate crypto;
-extern crate rand;
 
-use std::io;
+use std::env;
+use std::path::Path;
+use std::fs::File;
+
+use std::io::BufReader;
+use std::io::BufRead;
+
 use crypto::sha2::Sha256;
 use crypto::digest::Digest;
+// use openssl::crypto::pkey::PKey;
+
+static DEFAULT_WALLET: &'static str = "./keys/public.pem";
 
 fn main() {
-    println!("input your message to encrypto:");
+    let args: Vec<String> = env::args().collect();
+    let path;
 
-    let mut guess = String::new();
+    if args.len() > 1 {
+        path = Path::new(&args[1]);
+    } else {
+        path = Path::new(&DEFAULT_WALLET);
+    }
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    let f = File::open(path).expect("file not found");
+    let file = BufReader::new(&f);
 
-    let hash = hash_worker(&guess);
-    println!("Hash: {}", hash);
+    for (_, line) in file.lines().enumerate() {
+        let l = line.unwrap();
+        println!("{}", l);
+    }
+
+    // let mut contents = String::new();
+    // file.read_to_string(&mut contents)
+    //     .expect("can't read contents");
+    //
+    // println!("Content:\n{}", contents);
 }
 
 fn hash_worker(message: &str) -> String {
