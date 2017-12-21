1f1f5e0897f88bf7b63b51cde25e23c2518ceec0
Jacob Zuo <chopinsky@live.com>
2017-12-20 20:22:53 -0600

    refactor wallet loader code

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -3,39 +3,63 @@ extern crate crypto;
 use std::env;
 use std::path::Path;
 use std::fs::File;
-
-use std::io::BufReader;
-use std::io::BufRead;
+use std::io::Read;
 
 use crypto::sha2::Sha256;
 use crypto::digest::Digest;
-// use openssl::crypto::pkey::PKey;
 
-static DEFAULT_WALLET: &'static str = "./keys/public.pem";
+static DEFAULT_WALLET_PEM: &'static str = "./keys/public.pem";
+static DEFAULT_WALLET_DER: &'static str = "./keys/public.der";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let path;
 
+    let der = load_wallet_der(args);
+    let mut count = 0;
+    for x in &der {
+        print!("{} ", format!("{:03}", x));
+        count += 1;
+        if (count % 16) == 0 {
+            println!();
+        }
+    }
+
+    // let pem = load_wallet_pem(args);
+    // print!("{}", pem);
+}
+
+fn load_wallet_der(args: Vec<String>) -> Vec<u8> {
+    let path;
     if args.len() > 1 {
         path = Path::new(&args[1]);
     } else {
-        path = Path::new(&DEFAULT_WALLET);
+        path = Path::new(&DEFAULT_WALLET_DER);
     }
 
-    let f = File::open(path).expect("file not found");
-    let file = BufReader::new(&f);
+    let mut file = File::open(path).expect("file not found");
+    let mut contents: Vec<u8> = Vec::new();
 
-    for (_, line) in file.lines().enumerate() {
-        let l = line.unwrap();
-        println!("{}", l);
+    file.read_to_end(&mut contents)
+        .expect("can't read contents");
+
+    return contents;
+}
+
+fn load_wallet_pem(args: Vec<String>) -> String {
+    let path;
+    if args.len() > 1 {
+        path = Path::new(&args[1]);
+    } else {
+        path = Path::new(&DEFAULT_WALLET_PEM);
     }
 
-    // let mut contents = String::new();
-    // file.read_to_string(&mut contents)
-    //     .expect("can't read contents");
-    //
-    // println!("Content:\n{}", contents);
+    let mut file = File::open(path).expect("file not found");
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    return contents;
 }
 
 fn hash_worker(message: &str) -> String {
